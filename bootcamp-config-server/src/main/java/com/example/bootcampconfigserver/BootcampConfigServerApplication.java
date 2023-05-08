8efba57925c7cdd3a5a6284fa4f3e9eab80edc07
antoniomaral2003 <antoniomaral2003@gmail.com>
2023-05-08 13:47:57 +0200

    commit inicial

---
+++ bootcamp-config-server/src/main/java/com/example/bootcampconfigserver/BootcampConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.bootcampconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class BootcampConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampConfigServerApplication.class, args);
+    }
+
+}
---
+++ bootcamp-config-service/src/main/java/com/example/bootcampconfigservice/BootcampConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootcampconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampConfigServiceApplication.class, args);
+    }
+
+}
